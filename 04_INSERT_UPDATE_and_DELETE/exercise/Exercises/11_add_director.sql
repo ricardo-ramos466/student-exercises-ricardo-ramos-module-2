0d49610ab0d1dffac294a03aef1df7bb9d301fa5
Ricardo Ramos <ricardo.r.p466@gmail.com>
2022-06-28 23:05:04 -0400

    all done

--- 04_INSERT_UPDATE_and_DELETE/exercise/Exercises/11_add_director.sql
+++ 04_INSERT_UPDATE_and_DELETE/exercise/Exercises/11_add_director.sql
@@ -1,2 +1,5 @@
 -- 11. Hollywood is remaking the classic movie "The Blob" and doesn't have a director yet. Add yourself to the person table, and assign yourself as the director of "The Blob" (the movie is already in the movie table). (1 record each)
-
+INSERT INTO person (person_name)
+VALUES ('Ricardo Ramos');
+UPDATE movie SET director_id = (SELECT person_id FROM person WHERE person_name = 'Ricardo Ramos')
+WHERE title = 'The Blob'
